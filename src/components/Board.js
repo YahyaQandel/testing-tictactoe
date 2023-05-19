042f02f34248dec8b39f4a17237d3a9ce4036880
YahyaQandel <yahya.qandel@gmail.com>
2023-05-19 18:57:57 +0300

    initial commit

--- src/components/Board.js
+++ src/components/Board.js
@@ -2,10 +2,13 @@ import React from "react";
 import Square from "./Square";
 
 const Board = ({ squares, onClick }) => (
-  <div className="board">
-    {squares.map((square, i) => (
-      <Square key={i} value={square} onClick={() => onClick(i)} />
-    ))}
+  <div>
+    <h1>Board</h1>
+    <div className="board">
+      {squares.map((square, i) => (
+        <Square key={i} value={square} onClick={() => onClick(i)} />
+      ))}
+    </div>
   </div>
 );
 
--- src/components/Game.js
+++ src/components/Game.js
@@ -39,14 +39,15 @@ const Game = () => {
 
   return (
     <>
-      <h1>React Tic Tac Toe - With Hooks</h1>
-      <Board squares={history[stepNumber]} onClick={handleClick} />
-      <div className="info-wrapper">
-        <div>
-          <h3>History</h3>
-          {renderMoves()}
+      <div className="parent-container">
+        <div className="info-wrapper">
+          <div>
+            <h3>History</h3>
+            {renderMoves()}
+          </div>
+          <h3>{winner ? "Winner: " + winner : "Next Player: " + xO}</h3>
         </div>
-        <h3>{winner ? "Winner: " + winner : "Next Player: " + xO}</h3>
+        <Board squares={history[stepNumber]} onClick={handleClick} />
       </div>
     </>
   );
